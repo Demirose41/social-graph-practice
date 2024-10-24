78d33c092e81c2be9137226bfc2d5205714c63ee
demirose41 <demirose41@gmail.com>
2024-10-23 18:23:26 -0700

    completed part 1

--- social.js
+++ social.js
@@ -8,23 +8,38 @@ class SocialNetwork {
   }
 
   addUser(name) {
-    // Your code here
+    this.currentID++
+    const newUser = {
+      id: this.currentID, 
+      name: name
+    }
+    this.users[this.currentID] = newUser;
+    this.follows[this.currentID] = new Set();
+    return newUser.id
   }
 
   getUser(userID) {
-    // Your code here
-  }
+    return this.users[userID] ? this.users[userID] : null
+   }
 
   follow(userID1, userID2) {
-    // Your code here
+    if(!this.getUser(userID1) || !this.getUser(userID2)) return false
+    this.follows[userID1].add(userID2)
+    return true
   }
 
   getFollows(userID) {
-    // Your code here
+    if(!this.getUser(userID)) return null;
+    return this.follows[userID];
   }
 
   getFollowers(userID) {
-    // Your code here
+    const followers = new Set()
+    for(let i = 1; i < this.currentID; i++){
+      if(this.follows[i].has(userID)) followers.add(i)
+    }
+  
+    return followers
   }
 
   getRecommendedFollows(userID, degrees) {
